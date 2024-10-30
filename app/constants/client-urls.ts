65ed84761616e06c8e39cff5ac4f01382cf5ed13
mehmetalikuyucu <mkuyucu443@gmail.com>
2024-10-30 20:31:28 +0300

    feat: Add client URLs and useLeaderboardQueries hook for fetching leaderboard data

---
+++ app/constants/client-urls.ts
@@ -0,0 +1,5 @@
+export const  CLIENT_URLS = {
+    base: 'http://localhost:3001/',
+    prizePool: 'leaderboard/prize-pool',
+    search: 'leaderboard/search',
+}
---
+++ app/hooks/use-leader-board-queries.ts
@@ -0,0 +1,52 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { CLIENT_URLS } from '../constants/client-urls';
+
+export type Player = {
+  id: string;
+  playerName: string;
+  score: number;
+  rank: number;
+  details: {
+    country: string;
+    money: string;
+    userId: string;
+    username: string;
+    name: string;
+    surname: string;
+  };
+};
+
+export type QueryResponse = {
+  data: Player[];
+};
+
+export const useLeaderboardQueries = (searchValue: string) => {
+  const prizePoolQuery = useQuery({
+    queryKey: ['prizePoolQuery'],
+    queryFn: async () => {
+      const response = await axios.get(CLIENT_URLS.base+CLIENT_URLS.prizePool);
+      return response.data;
+    },
+  });
+
+  const playerQuery = useQuery({
+    queryKey: ['playersQuery', searchValue],
+    queryFn: async () => {
+      const response = await axios.get<QueryResponse>(
+        `${CLIENT_URLS.base+CLIENT_URLS.search}?playerName=${searchValue}`
+      );
+      return response.data;
+    },
+    staleTime: 300,
+  });
+
+  return {
+    prizePool: prizePoolQuery.data,
+    players: playerQuery.data?.data || [],
+    isPrizePoolLoading: prizePoolQuery.isLoading,
+    isPlayersLoading: playerQuery.isLoading,
+    isError: prizePoolQuery.isError || playerQuery.isError,
+    error: prizePoolQuery.error || playerQuery.error,
+  };
+};
