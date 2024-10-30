72b584831e64cd6a11fe31fc7f72b9be65f88b23
mehmetalikuyucu <mkuyucu443@gmail.com>
2024-10-30 20:23:56 +0300

    chore: Add remote image pattern for Panteon logo in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns:[
+      {
+        hostname: "www.panteon.games",
+        port: '',
+        protocol: "https",
+        pathname: "/wp-content/themes/panteon/assets/img/logo@2x.png"
+        
+      }
+    ]
+  },
 };
 
 export default nextConfig;
