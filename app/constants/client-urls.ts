043f42fb755bb79a5835cc0f277b9957b300a0d6
mehmetalikuyucu <mkuyucu443@gmail.com>
2024-10-31 12:57:30 +0300

    refactor: Update base URL in client-urls.ts

--- app/constants/client-urls.ts
+++ app/constants/client-urls.ts
@@ -1,5 +1,5 @@
-export const  CLIENT_URLS = {
-    base: 'http://localhost:3001/',
-    prizePool: 'leaderboard/prize-pool',
-    search: 'leaderboard/search',
-}
+export const CLIENT_URLS = {
+  base: "http://31.57.154.240:3001/",
+  prizePool: "leaderboard/prize-pool",
+  search: "leaderboard/search",
+};
